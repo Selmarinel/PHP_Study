d193e004f7d632250f27445070045942cf619e17
Selmarinel <selmarinel@gmail.com>
2015-09-12 23:57:01 +0300

    auth test

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
-<?PHP
-echo "Резинка жевательная Orbit Eucalipt драже<br>";
-echo "Чай Лисма Каркаде м/у<br>";
-echo "Резинка жевательная Orbit Whit Fresh Mint драже<br>";
-echo "Напиток энергетический Non Stop сильногазиров. ж/б<br>";
+
+<?php
+//$username="Admin";
+//$pass="masterkey";
+if (($_POST["user"]==="Admin") && ($_POST["pass"]==="masterkey"))
+{
+    echo "<PRE>";
+    echo "You are aauth now!";
+    echo "<br/> " . $_POST["user"];
+    echo "<br/>"  . $_POST["pass"];
+    echo "</PRE>";
+} else
+{
+    $io=$_POST["go"];
+    if (isset($io)){echo "<h1>Incorect pass or login</h1>"; }
+    echo "<form action='index.php' METHOD=POST>
+    Login:<input type='text' name ='user' />
+    <br>";
+    echo "
+    Password:<input type='text' name = 'pass' />
+    <br>";
+    echo "<input type = 'submit' id='go' name = 'go'/>";
+    echo "</form>";
+}
 ?>
